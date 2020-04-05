655bd81920310a33c04aff1a0b7faa932ed3b5fb
matheusegredo <mdelsegredo@hotmail.com>
2020-04-04 21:54:22 -0300

    Menu adicionado e correção no layout thankyou.html

--- models/match_users.py
+++ models/match_users.py
@@ -2,6 +2,6 @@ class Match_Users:
     
     def __init__(self, nome, ramo, telefone, email):
         self.nome = nome
-        self.ramo = ramo
+        self.ramo = ', '.join(["%s" % v for v in ramo])
         self.telefone = telefone
         self.email = email
